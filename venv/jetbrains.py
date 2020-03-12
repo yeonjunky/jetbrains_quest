f3c57728cd69304038aa0bd664d7f4526d76a7a9
yeonjunky <yeonjunky@gmail.com>
2020-03-12 20:28:44 +0900

    jetbrains quest

--- venv/jetbrains.py
+++ venv/jetbrains.py
@@ -1,3 +1,19 @@
-def prime_number(x):
-    for i in range(5000):
-        
+def is_prime_number(x):
+
+    if x <= 0:
+        return False
+
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+
+    return True
+
+prime_number = []
+
+for i in range(500, 5001):
+    if is_prime_number(i) == True:
+        prime_number.append(i)
+
+print(len(prime_number))
+#https://jb.gg/574
